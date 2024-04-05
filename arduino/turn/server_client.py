1c2e4523afda0b23d315c283763e5ead84338321
amadeok <43666702+amadeok@users.noreply.github.com>
2024-04-05 18:11:51 +0200

    a

---
+++ arduino/turn/server_client.py
@@ -0,0 +1,25 @@
+
+
+#import serial
+import time, random
+#import autopyBot
+
+#a = autopyBot.autopy.autopy(r"F:\all\GitHub\aws-python\arduino\turn\imgs")
+import ArdClick
+ac = ArdClick.ardclick.ardclick()
+ac.init()
+n= 0
+updateIntervalCode =  40010
+setBeepOnBlink =  40011
+
+while 1:
+    ac.write_custom(updateIntervalCode, [2])
+    #ac.write_custom(setBeepOnBlink, [0])
+
+    # ac.mouse_move((n,n))
+    break
+    n+=1
+    ac.mouse_move((random.randint(0, 3000), random.randint(0, 2000)))
+    time.sleep(0.1)
+    if n > 10:break
+
