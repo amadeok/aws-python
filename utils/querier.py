0775a10fcb2fdb647c9933dfeb9a31a37b4c5a51
amadeok <43666702+amadeok@users.noreply.github.com>
2025-02-23 10:12:26 +0100

    a

--- utils/querier.py
+++ utils/querier.py
@@ -352,8 +352,8 @@ if __name__ == "__main__":
 
     uri = os.getenv("MONGODB_URI")
 
-
-
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(script_dir)
 
     root = tk.Tk()
     root.title("JSON Editor")
